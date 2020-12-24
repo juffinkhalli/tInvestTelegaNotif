e8aed4497dc4b098f23c6e86dd0c8492deeb38d0
juffinkhalli <kravases@mail.ru>
2020-12-24 23:05:42 +0300

    tInvest notification bot v.1.0

---
+++ config.py
@@ -0,0 +1,4 @@
+tInv_token = 't.dYFekeZF3D1qptIXgB19ajLQWnSMYLQ4CW93769UWikV6vVKmKVhzO-vIks4xd2y0lgYKWRGV2TbyZBcX3o8mw'
+telegram_token = '1433597282:AAH3I0oZ_8xb71NF0GWiIGQ85ZQofCMrlLc'
+my_account_id = '2019385174'
+my_chat_id = '217566304'
---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import requests
+# import config
+import threading
+import telegram
+from telegram.ext import Updater
+
+tInv_token = ''
+telegram_token = ''
+my_chat_id = ''
+
+
+def set_config():
+    global tInv_token
+    global telegram_token
+    global my_chat_id
+    if os.environ.get('HEROKU', False):
+        tInv_token = os.getenv('tInv_token')
+        telegram_token = os.getenv('telegram_token')
+        my_chat_id = os.getenv('my_chat_id')
+    else:
+        # tInv_token = config.tInv_token
+        # telegram_token = config.telegram_token
+        # my_chat_id = config.my_chat_id
+        print('Config installed')
+
+
+def getPortfolio():
+    req = requests.get('https://api-invest.tinkoff.ru/openapi/portfolio',
+                       headers={'Authorization': 'Bearer ' + tInv_token})
+    temp_res = req.json()['payload']['positions']
+    temp_str = ''
+    for i in temp_res:
+        if i['instrumentType'] != 'Currency':
+            temp_str += (i['name'] + ' ' +
+                         str(int(i['balance']) * int(i['averagePositionPrice']['value'])) + ' ' +
+                         str(i['expectedYield']['value']) + ' ' +
+                         i['averagePositionPrice']['currency'] + '\n')
+        else:
+            temp_str += (i['name'] + ' ' +
+                         str(i['balance']) + ' ' +
+                         str(i['expectedYield']['value']) + ' ' +
+                         i['averagePositionPrice']['currency'] + '\n')
+    return temp_str
+
+
+def sendPortfolio():
+    print('message sending...')
+    bot.send_message(my_chat_id, text=getPortfolio())
+    threading.Timer(900, sendPortfolio).start()
+    print('message send.')
+
+
+set_config()
+bot = telegram.Bot(telegram_token)
+bot.send_message(my_chat_id, text='START')
+sendPortfolio()
+updater = Updater(telegram_token, use_context=True)
+updater.start_polling()
+updater.idle()
